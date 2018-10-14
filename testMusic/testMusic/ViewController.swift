b4cec0523e037b7ea3173f7bff41cb910de2fc65
Ryan <ryan@macbooks-mbp.wifi.bcit.ca>
2018-10-14 15:44:30 -0700

    Added play, pause, and volume

--- testMusic/testMusic/ViewController.swift
+++ testMusic/testMusic/ViewController.swift
@@ -7,14 +7,69 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    // The ! means that we know that this song exsist because we placed it in our folder
+    // import the song by drag and dropping the song into the folder and add it to the build phases's
+    // copy bundle sources
+    private var song = URL.init(fileURLWithPath: Bundle.main.path(forResource: "05 Bambi", ofType: "m4a")!)
+    
+    // We create a AVAudioPlayer object that plays music from a file
+    private var songPlayer = AVAudioPlayer()
+    
+    
+    @IBOutlet weak var volume: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        prepareSongAndSession()
     }
-
-
+    
+    // Prepares our song
+    private func prepareSongAndSession() {
+        do {
+            songPlayer = try AVAudioPlayer(contentsOf: song)
+            
+            // Prepares the audio player for playback by preloading its buffers
+            songPlayer.prepareToPlay()
+            
+            prepareSession()
+        } catch let songPlayError {
+            print(songPlayError)
+        }
+    }
+    
+    // Prepares our audio session
+    private func prepareSession() {
+        // Creating an object that is an instance of the AVAudioSession
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            // This is the one way for it to work in xcode 10
+            // first argument is playing back the song
+            // we'll leave mode to default
+            try audioSession.setCategory(.playback, mode: .default)
+            try audioSession.setActive(true)
+        } catch let sessionError {
+            print(sessionError)
+        }
+    }
+    // Plays the song
+    @IBAction func playSong(_ sender: Any) {
+        songPlayer.play()
+    }
+    
+    // Pauses the song
+    @IBAction func stopSong(_ sender: Any) {
+        songPlayer.pause()
+    }
+    
+    // Volume of the song
+    @IBAction func volumeSlider(_ sender: Any) {
+        songPlayer.volume = volume.value
+    }
 }
 

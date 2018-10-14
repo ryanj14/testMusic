833094d23695673184cad96a51a53053ce4fa7e0
Ryan <ryan@Macbooks-MacBook-Pro.local>
2018-10-14 15:19:31 -0700

    Initial project:

---
+++ testMusic/testMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testMusic
+//
+//  Created by Ryan on 2018-10-14.
+//  Copyright © 2018 Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
